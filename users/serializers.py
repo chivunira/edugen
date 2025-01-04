0c0a60a4f68b1cc5b131d9d8d7be7da50835c089
chivunira <emmanuelchivunira@gmail.com>
2025-01-04 23:05:31 +0300

    create endpoints and serializer for profile section

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -27,3 +30,36 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             profile_photo=validated_data.get('profile_photo'),
         )
         return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'grade',
+            'profile_photo',
+            'date_joined'
+        ]
+        read_only_fields = ['id', 'email', 'date_joined']
+
+
+class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'profile_photo']
+
+    def update(self, instance, validated_data):
+        if 'profile_photo' in validated_data:
+            # Delete old photo if it exists
+            if instance.profile_photo:
+                instance.profile_photo.delete(save=False)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
